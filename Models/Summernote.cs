70845d59ce8e7df468b747e6050b895af0f22f53
duonga6 <shenmurai1st@gmail.com>
2023-08-16 15:56:37 +0700

    addSummernote

---
+++ Models/Summernote.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Summernote
+    {
+        public Summernote(string id, bool loadLibrary = true, int height = 120)
+        {
+            Id = id;
+            LoadLibrary = loadLibrary;
+            Height = height;
+        }
+
+        public string Id { set; get; }
+        public bool LoadLibrary { set; get; }
+        public int Height { set; get; }
+        public string Toolbar { set; get; } = @"
+            [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        ";
+    }
+}
